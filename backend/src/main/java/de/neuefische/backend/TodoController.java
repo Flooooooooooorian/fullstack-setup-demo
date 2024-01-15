6a72d91b85b272531d0a0042b5fd3fe38c47586b
Flooooooooooorian <florian.florianweber.weber@googlemail.com>
2024-01-15 15:46:24 +0100

    Add Todo-Gallery Endpoint

---
+++ backend/src/main/java/de/neuefische/backend/TodoController.java
@@ -0,0 +1,21 @@
+package de.neuefische.backend;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/todo")
+@RequiredArgsConstructor
+public class TodoController {
+
+    private final TodoService todoService;
+
+    @GetMapping
+    List<Todo> getAllTodos() {
+        return todoService.getAllTodos();
+    }
+}
